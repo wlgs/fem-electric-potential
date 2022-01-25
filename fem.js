2575afdbf76b1e8e76efe51220bb5be347c8e77b
Mikołaj Wielgos <wielgosmikolaj@gmail.com>
2022-01-25 02:19:28 +0100

    problem with shift

--- fem.js
+++ fem.js
@@ -1,3 +1,7 @@
+
+/* IMPORTED SOLVER AND GAUSSLEGENDRE*/
+
+
 function Mat(data, mirror) {
     // Clone the original matrix
     this.data = new Array(data.length);
@@ -113,17 +117,87 @@ function solve(A, b) {
     }
     return result;
 }
+/* IMPORTED SOLVER AND GAUSSLEGENDRE END*/
+
+/* GRAPHS AND BUTTON HANDLERS */
+
+function drawGraph(n) {
+    myChart = new Chart(document.getElementById("femChart").getContext("2d"), {
+        type: "line",
+        data: {
+            datasets: [
+                {
+                    label: "calculated FEM",
+                    data: fem(n),
+                    backgroundColor: "#041C32",
+                    borderColor: "#041C32",
+                    fill: false,
+                },
+            ],
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                xAxes: [
+                    {
+                        type: "linear",
+                    },
+                ],
+            },
+            tooltips: {
+                enabled: false,
+            },
+            animation: false,
+            hover: { mode: null },
+            elements: {
+                point: {
+                    radius: 3,
+                },
+            },
+        },
+    });
+}
+
+var myChart = null
+var cnt = 0;
+var snackbarContainer = document.querySelector("#information");
+window.onload = () => {
+    document.getElementById("btn").addEventListener("click", () => {
+        if (document.getElementById("inputValue").value != "") {
+            var date1 = new Date();
+
+            drawGraph(Number(document.getElementById("inputValue").value));
+            var date2 = new Date();
+            var diff = date2 - date1;
+            var data = { message: "Calculated in " + diff + "ms" };
+            snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        } else {
+            var data = { message: "Input some values first!" };
+            snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        }
+    });
+
+    document.getElementById("switch-1").addEventListener("click",()=>{
+        if (cnt%2==0)
+            myChart.options.elements.point.radius = 0;
+        else
+            myChart.options.elements.point.radius = 3;
+        cnt += 1
+        myChart.update();
+    });
+};
+
+/* GRAPHS AND BUTTON HANDLERS END*/
+
+/* REAL CODE BEGINS HERE */
 
 const integrate = (fn, a, b) => gaussLegendre(fn, a, b, 5);
-
-/* const */
 
 const rho = 1;
 const a = 0;
 const b = 3;
 const length = Math.abs(b - a);
-
-/* functions */
 
 function e_r(x) {
     if (x >= 0 && x <= 1) return 10;
@@ -138,7 +212,7 @@ function e(i, x, n) {
     let xi3 = h * (i + 1);
 
     if (x > xi1 && x <= xi2) return (x - xi1) / h;
-    else if (x > xi2 && x < xi3) return (xi3 - x) / h;
+    else if (x > xi2 && x <= xi3) return (xi3 - x) / h;
     return 0;
 }
 
@@ -193,100 +267,30 @@ function fem(n) {
 
     for (let i = 0; i <= n - 1; i++)
         for (let j = 0; j <= n - 1; j++) {
-            if (Math.abs(i - j) > 1) continue;
-            matrixB[i][j] = B(i, j, n);
+            if(Math.abs(i-j)<=1)
+                matrixB[i][j] = B(i, j, n);
         }
     for (let i = 0; i <= n - 1; i++) {
         matrixL[i] = L2(i, n);
     }
     var matrixW = solve(matrixB, matrixL);
+    console.log(matrixW)
 
-    var matrixX = new Array(n);
-    for (let i = 0; i <= n - 1; i++) {
-        matrixX[i] = i * h;
+    var finalRes = new Array()
+    for(var i = 0; i <= n-1; i++){
+        finalRes.push({
+            x: i*h,
+            y: getY(matrixW, i*h, n)
+        })
     }
-    // console.log(matrixB)
-    // console.log(matrixL)
-    // console.log(matrixX)
-    // console.log("----------")
-
-    return {
-        fem: matrixX.map((x, i) => ({
-            x: x,
-            y: matrixW[i] + 2 * e(n, x, n),
-        })),
-    };
+    return finalRes
 }
-
-
-
-function drawGraph(n) {
-    myChart = new Chart(document.getElementById("femChart").getContext("2d"), {
-        type: "line",
-        data: {
-            datasets: [
-                {
-                    label: "equation value",
-                    data: fem(n).fem,
-                    backgroundColor: "#041C32",
-                    borderColor: "#041C32",
-                    fill: false,
-                },
-            ],
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            scales: {
-                xAxes: [
-                    {
-                        type: "linear",
-                    },
-                ],
-            },
-            tooltips: {
-                enabled: false,
-            },
-            animation: false,
-            hover: { mode: null },
-            elements: {
-                point: {
-                    radius: 3,
-                },
-            },
-        },
-    });
+function getY(matrixW, x, n){
+    var res = 0
+    for(var i =0; i<=n-1; i++){
+        res += matrixW[i]*e(i,x,n)
+    }
+    res += 2 * e(n, x, n)
+    console.log(2 * e(n, x, n))
+    return res
 }
-
-var myChart = null
-console.log(myChart)
-var cnt = 0;
-var snackbarContainer = document.querySelector("#information");
-window.onload = () => {
-    document.getElementById("btn").addEventListener("click", () => {
-        if (document.getElementById("inputValue").value != "") {
-            var date1 = new Date();
-
-            drawGraph(Number(document.getElementById("inputValue").value));
-            var date2 = new Date();
-            var diff = date2 - date1;
-            var data = { message: "Calculated in " + diff + "ms" };
-            snackbarContainer.MaterialSnackbar.showSnackbar(data);
-        } else {
-            var data = { message: "Input some values first!" };
-            snackbarContainer.MaterialSnackbar.showSnackbar(data);
-        }
-    });
-
-    document.getElementById("switch-1").addEventListener("click",()=>{
-        if (cnt%2==0)
-            myChart.options.elements.point.radius = 0;
-        else
-            myChart.options.elements.point.radius = 3;
-        cnt += 1
-        myChart.update();
-    });
-
-
-};
-
